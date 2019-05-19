f726b68f2c3ed9254b68c5e0e51734a78201efa8
Jay <hinds.jason@gmail.com>
2019-05-19 15:47:38 -0400

    Update runWithDebugger.js
    added semi-colon

--- runWithDebugger/runWithDebugger.js
+++ runWithDebugger/runWithDebugger.js
@@ -2,7 +2,7 @@
 function runWithDebugger(callback, optionalArray) {
   debugger;
   if (optionalArray && Array.isArray(optionalArray)) {
-    callback.apply(this, optionalArray)
+    callback.apply(this, optionalArray);
   } else {
     callback();
   }
