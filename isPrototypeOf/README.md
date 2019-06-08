4c232d973d6689a268e3cd3b0e3c715bd9e1bd55
Jay <hinds.jason@gmail.com>
2019-06-08 19:27:26 -0400

    Add files via upload

---
+++ isPrototypeOf/README.md
@@ -0,0 +1,5 @@
+Solution for beast challenge #5 - isPrototypeOf
+
+Task is to write a function that works just like the Object.prototype.isPrototypeOf method.
+
+Tests were run and passed to ensure all requirements are met.  Testing library based on the tinytest.js library.
---
+++ isPrototypeOf/isPrototypeOf.js
@@ -0,0 +1,40 @@
+// Function Signature:
+  // isPrototypeOf(prototypeObj, object);
+
+// Parameters:
+  // prototypeObj >>> the object prototype in which another object may be linked/based upon
+  // object >>> the object whose prototype chain will be searched
+
+// Return Value:
+  // returns a Boolean indicating whether the calling object lies in the prototype chain of the specified object
+
+// Description:
+  // to check whether or not an object exists within another object's prototype chain
+  // a TypeError is thrown if prototypeObj is undefined or null
+
+// ****************************
+
+// >>> goal: to go through object prototype links and return true if there are matches as per our arguments
+// >>> look to use recursion as there may be many depths to evaluate
+
+
+function isPrototypeOf(prototypeObj, obj) {
+  var objectToBeTested = Object.getPrototypeOf(obj);
+  
+  if (prototypeObj === undefined) {
+    throw new TypeError('Cannot read property "isPrototypeOf" of undefined');
+  }
+  if (prototypeObj === null) {
+    throw new TypeError('Cannot read property "isPrototypeOf" of null');
+  }
+  
+  if (prototypeObj === objectToBeTested) {
+    return true;
+  } else {
+    if (objectToBeTested === null) {
+      return false;
+    } else {
+    return isPrototypeOf(prototypeObj, objectToBeTested);
+    }
+  }
+}
